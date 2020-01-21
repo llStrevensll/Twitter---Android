72a23048c98b4782e4c2beccf6e19defc55b584f
Steven <anguelr12@hotmail.com>
2020-01-21 12:11:01 -0500

    Servicio Retrofit Login - Registro

---
+++ app/src/main/java/com/example/minitwitter/retrofit/MiniTwitterService.java
@@ -0,0 +1,19 @@
+package com.example.minitwitter.retrofit;
+
+import com.example.minitwitter.RequestLogin;
+import com.example.minitwitter.RequestSignUp;
+import com.example.minitwitter.ResponseAuth;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface MiniTwitterService {
+
+    @POST("/auth/login")
+    Call<ResponseAuth> doLogin(@Body RequestLogin requestLogin);
+
+    @POST("/auth/signup")
+    Call<ResponseAuth> doSignUp(@Body RequestSignUp requestSignUp);
+
+}
